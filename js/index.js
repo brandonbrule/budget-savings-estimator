5a8384b2dc877bd392fe37abc31f2984a9fd5f5f
brandonbrule <brandonbrule@gmail.com>
2015-03-10 22:39:15 -0400

    much nicer ui, cleanup of code cause it be old.

--- js/index.js
+++ js/index.js
@@ -7,14 +7,6 @@ function elById(arg){ return document.getElementById(arg); }
 // Return Value and Display to HTML Elements Round to nearest 0.00x
 function round(arg){ return Math.round(arg * 100) / 100; }
 
-// Display Print
-function dp(context){
-  var container = elById('results');
-  var el = document.createElement('div');     
-  el.innerHTML = context;
-  container.appendChild( el );
-}
-
 // Useful Math Functions
 // -------------------------- //
 function monthToYear(value) { return round(value * 12) ; }
@@ -24,10 +16,10 @@ function daily(value){ return round(value / 365); }
 
 
 function annualBreakdown(type,value){
-  var years = 'Yearly ' + type;
-  var months = 'Monthly ' + type;
-  var weeks = 'Weekly ' + type;
-  var days = 'Daily ' + type;
+  var years = 'Yearly';
+  var months = 'Monthly';
+  var weeks = 'Weekly';
+  var days = 'Daily';
 
   var annualBreakdown = {};
   annualBreakdown[years] = value;
@@ -38,48 +30,170 @@ function annualBreakdown(type,value){
   return annualBreakdown;
 }
 
-function yearlyBreakdown(label, value){
-  var yearsObj = new Array();
-  for(var i = 1, years = 10; i < years; i++){
+function yearlyBreakdown(label, value, length_of_savings){
+  var yearsObj = {};
+  var value = value;
+  for(var i = 1, years = length_of_savings; i <= years; i++){
     var obj = {};
-    obj.label = label + ' - Yr ' + i + ' - ';
-    obj.value = value * i;
-    yearsObj.push(obj);    
+    yearsObj[label + '-' + i] = value * i;  
   }
   return yearsObj;
 }
 
 
-function displayBreakdown(breakdown){
-  var keysArr = [];
-  for (var key in breakdown) {
-     if (breakdown.hasOwnProperty(key)) {
-       
-       var together = '';
-       var b = breakdown[key];
-       var flag = false;
-       
-       for( var k in b){
-          if (b.hasOwnProperty(k)) {
-             var label = k;
-             var keyNOT = b[k];
-            together =  together + keyNOT + '';
-            flag = true;
-          }
-       }
+function displayBreakdown(heading, object){
 
-       if (!flag) {
-         var keys = key;
-         var value = '<strong>' + breakdown[key] + '</strong>';
-         together = key + value;
-       }
-       
-       keysArr.push('<p>' + together + '</p>');
-     }
+  var container = elById('results');
+  var el = document.createElement('div');
+  var header = document.createElement('h3');
+  var list_container = document.createElement('ol');
+
+  // Heading
+  header.innerHTML = heading;
+  el.appendChild(header);
+
+  for (var key in object) {
+    if (object.hasOwnProperty(key)) {
+
+      var list_item = document.createElement('li');
+      var value = '<strong>' + object[key] + '</strong>';
+         
+      list_item.innerHTML = key + ' ' + value;
+      list_container.appendChild( list_item );
+
+    }
+   
   }
-  keysArr = keysArr.join("");
-  return keysArr;
-}
+
+  el.appendChild(list_container);
+  container.appendChild(el);
+};
+
+
+
+
+var Investing = (function () {
+  
+  var investing_container = document.getElementById('investing-container');
+  var data = {};
+  
+  var default_config = {
+    principle_value : 5000,
+    monthly_contribution: 100,
+    annual_interest: 0.05,
+    times_compounded_per_year: 12,
+    length_of_investment: 10
+  };
+  
+  var initialSavingsCompounded = function(P, base, exponent) {
+    var result = 1;
+    var arr = []
+
+    for (var i = 0; i < exponent; i++){
+      result *= base;
+      arr.push( Math.round( result * P * 100 ) / 100 );
+    }
+    data['Starting-Savings-Compounded-Breakdown'] = arr;
+    return arr;
+  };
+
+  var investmentsCompounded = function(PMT, base, exponent, r, n){
+    //PMT * ( Math.pow( (1 + r / n),n * (t)) - 1 ) / (r/n)
+    var result = 1;
+    var arr = []
+
+    for (var i = 0; i < exponent; i++){
+      result *= base;
+      arr.push( Math.round( ( result - 1 ) * PMT / (r/n) * 100 ) / 100 );
+    }
+    data['Contributions-Compounded-Breakdown'] = arr;
+    // First savings is 0, then after the first month your savings per month
+    arr.unshift(0);
+    return arr;
+  };
+  
+  var totalCompounded = function(arr1, arr2){
+    var arr = [];
+    for (var i = 0, len = arr1.length - 1; i < len; i++){
+      arr.push( Math.round((arr1[i] + arr2[i])*100.0)/100.0 );
+    }
+    return arr;
+    
+  };
+
+
+  var compounded_savings = function(config){
+    var A; //the future value of the investment/loan, including interest
+    var P = config.principle_value; //the principal investment amount (the initial deposit or loan amount)
+    var PMT = config.monthly_contribution //the monthly payment
+    var r = config.annual_interest; //the annual interest rate (decimal)
+    var n =  config.times_compounded_per_year;//the number of times that interest is compounded per year
+    var t = config.length_of_investment;//the number of years the money is invested or borrowed for
+    
+    // Starting Savings Compounded
+    var present_value = P;
+    var principal_compound_interest = P * Math.pow((1.0+r / n),n * (t));
+    var total_interest_from_principal = principal_compound_interest - present_value;
+    
+    principal_compound_interest = Math.round(principal_compound_interest*100.0)/100.0;
+    total_interest_from_principal  = Math.round(total_interest_from_principal*100.0)/100.0;
+    
+    
+    //its.a( principal_compound_breakdown );
+    
+    // Startings Savings
+    data['Starting-Investment'] = P;
+    data['Monthly-Contributions'] = PMT;
+    data['Years-Compounded'] = t;
+    data['Compounded-Per-Year'] = n;
+    
+    data['Starting-Investment-Compounded'] = principal_compound_interest;
+    
+    // Breakdown of Savings Over Time of Initial Investment
+    var principal_compound_breakdown = initialSavingsCompounded( P, (1.0+r / n), n * t );
+    
+    
+    // Total Monthly Contributions Compounded
+    var total_monthly_contributions = (  PMT * ( Math.pow( (1 + r / n),n * (t)) - 1 ) / (r/n)  );
+    
+    data['Total-Monthly-Contibutions'] = Math.round( total_monthly_contributions * 100) / 100;
+    
+    var principal_contributions_breakdown = investmentsCompounded( PMT, (1.0+r / n), n * t, r, n );
+    
+    
+    // Interest of Monthly payments + Amount Saved
+    var total_savings = principal_compound_interest + total_monthly_contributions;
+    total_savings = Math.round( total_savings * 100 ) / 100;
+    data['Total-Savings'] = total_savings;
+    
+    
+    
+    var test = totalCompounded(principal_contributions_breakdown, principal_compound_breakdown);
+    data['Total-Compounded'] = test;
+    
+    return data;
+    
+  };
+
+  var init = function (config) {
+    var config = typeof config !== 'undefined' ? config : default_config;
+    compounded_savings(config);
+  };
+  
+  return {
+    init: init,
+    compounded_savings: compounded_savings
+  };
+
+})();
+
+
+
+
+
+
+
+
 
 // Simple Budget Object
 var SimpleBudget = {
@@ -108,16 +222,9 @@ var SimpleBudget = {
     var leftover = annualBreakdown('Leftovers', leftovers)
     return leftover;
   },
-  overTime: function(heading, value){
-    var yearLabel = 'Year';
-    var years = yearlyBreakdown(heading, value);
+  overTime: function(heading, value, length_of_savings){
+    var years = yearlyBreakdown(heading, value, length_of_savings);
     return years;
-  },
-  display: function(heading, object){
-    var income = object,
-        heading = '<h3>' + heading + '</h3>'
-        income = displayBreakdown(income);
-    dp(heading + income);
   },
   calculate: function(){
       
@@ -133,7 +240,9 @@ var SimpleBudget = {
         savings = elById('savings').value * 0.01,
         savingsOverTime,
         leftover,
-        interest = elById('interest').value * 0.01;
+        initialSavings = elById('initial-savings').value;
+        interest = elById('interest').value * 0.01,
+        length_of_savings = elById('length-of-savings').value;
     
     // How often you get paid
     if(annualIncome == 'once-month'){
@@ -147,42 +256,71 @@ var SimpleBudget = {
     }
     
     income = this.income(annualIncome);
-    this.display('Income Breakdown', income);
+    displayBreakdown('Income', income);
     
     //Payments
     payments = this.payments(annualPayments);
-    this.display('Payment Breakdown', payments);
+    displayBreakdown('Payments', payments);
     
     // Savings
     annualSavings = (annualIncome - annualPayments) * savings;
     savings = this.savings(annualSavings);
-    this.display('Savings Breakdown', savings);
+    displayBreakdown('Savings', savings);
     
     // Left over
     leftover = (annualIncome - annualPayments);
     leftover = round(leftover - (leftover * elById('savings').value * 0.01));
     leftover = this.leftOver(leftover);
-    this.display('Leftover Breakdown', leftover);
+    displayBreakdown('Leftover', leftover);
     
     
     // Over Time
-    incomeOverTime = this.overTime('Income', annualIncome);
-    this.display('Income Over Time', incomeOverTime);
+    incomeOverTime = this.overTime('Year', annualIncome, length_of_savings);
+    displayBreakdown('Income', incomeOverTime);
     
+    // Payments over time
+    paymentsOverTime = this.overTime('Year', annualPayments, length_of_savings);
+    displayBreakdown('Payments', paymentsOverTime);
+
     // Savings over time
-    savingsOverTime = this.overTime('Contributions', annualSavings);
-    this.display('Contributions Over Time', savingsOverTime);
+    savingsOverTime = this.overTime('Year', annualSavings, length_of_savings);
+    displayBreakdown('Savings', savingsOverTime);
+
+    // Leftover over time
+    leftoverOverTime = this.overTime('Year', leftover.Yearly, length_of_savings);
+    displayBreakdown('Leftover', leftoverOverTime);
+
+
     
-    
-    
-    
-    //console.log(income);
-    //console.log(payments);
-    //console.log(savings);
-    //console.log(leftover);
+
+    // Savings with Interest
+    var custom_config = {
+      principle_value : initialSavings,
+      monthly_contribution: savings.Yearly,
+      annual_interest: interest,
+      times_compounded_per_year: 1,
+      length_of_investment: length_of_savings
+    };
+
+    // Don't do anything if there's no interest to compound
+    if (interest > 0){
+      var savingsWithInterest = Investing.compounded_savings(custom_config);
+      displayBreakdown('With Interest', savingsWithInterest['Total-Compounded']);
+    }
     
   }
 };
+
+
+
+
+
+
+
+
+
+
+    
 
 // On Submit
 elById("submit").onclick = function () {
